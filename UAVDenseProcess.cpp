db5b70b0d9d23a0f5153ddd11d93d0395170c577
Frank Wu <wuwei_cug@163.com>
2017-12-01 09:03:41 +0800

    change for travis

--- UAVDenseProcess.cpp
+++ UAVDenseProcess.cpp
@@ -5,7 +5,7 @@
 #include "third_party/stlplus3/filesystemSimplified/file_system.hpp"
 #include <boost/program_options.hpp>
 #include <omp.h>
-#include <OpenMVS/MVS.h>
+#include "MVS.h"
 
 void UAVDenseProcess::UAVDP_MVSProcInitialize() {
 
